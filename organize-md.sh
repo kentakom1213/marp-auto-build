2197ecf63bd6c9bf465943bb7fe65cce436c72f8
kentakom1213 <kentakom1213@gmail.com>
2023-10-18 02:38:37 +0900

    [fix] スライドの削除時、pagesの方に削除が反映されない問題を修正

--- organize-md.sh
+++ organize-md.sh
@@ -1,17 +1,20 @@
 #!/bin/zsh
 
-# mermaidのCDNリンク
 local MARP_CDN='
 <!-- mermaid.js -->
 <script src="https://unpkg.com/mermaid@8.1.0/dist/mermaid.min.js"></script>
 <script>mermaid.initialize({startOnLoad:true});</script>'
 
+# slidesのリセット
+rm -rf slides && mkdir -p slides/images
+touch slides/images/.gitkeep
+
+# magesのリセット
+rm -rf output && mkdir -p output/images
+touch output/images/.gitkeep
+
 # slides/index.mdのリセット
 echo '# slides' > slides/index.md
-
-# slides/imagesのリセット
-rm -rf slides/images && mkdir slides/images
-rm -rf output/images && mkdir output/images
 
 # ディレクトリのみ取り出し
 for dir in $(ls -l | grep '^d' | awk '{print $9}')
@@ -19,7 +22,6 @@ do
     if [[ ! $dir =~ "slides|output" ]] then
         # markdownファイルのコピー
         cat "$dir/slide.md" > "slides/$dir.md"
-        # mermaidのCDNを追加
         echo $MARP_CDN >> "slides/$dir.md"
         # indexに追記
         echo "- [$dir](./$dir) ([PDF](./$dir.pdf))" >> 'slides/index.md'
